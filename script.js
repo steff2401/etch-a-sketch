47a18f2fec4eca01f2652b0f08fc343e7656f9ef
steff2401 <stephank@hotmail.no>
2023-07-09 17:17:32 +0200

    Make colors random

--- script.js
+++ script.js
@@ -18,13 +18,22 @@ function createGrid(gridSize) {
             gridSquare.style.height = `${grid.clientWidth / gridSize}px`;
     
             gridSquare.addEventListener("mouseover", () => {
-                gridSquare.style.backgroundColor = "black";  
+                gridSquare.style.backgroundColor = getRandomColor();  
             });
             
             grid.appendChild(gridSquare);
         }
     }
 }
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 createGrid(gridSize);
 
